824b015184aeb7e6a268871239b16886f8c6eb01
Marina Ferreira <33575258+loadingerror@users.noreply.github.com>
2023-11-13 23:06:13 -0300

    update api

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\GitHub\teste_seedz\agro_analisys\venv
+set VIRTUAL_ENV=C:\Users\55489\Documents\GitHub\agro_analysis\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
